34def1e4c7c2efab4a7792605d78660a6b2693cc
Alec Guintu <aeguintu@gmail.com>
2012-02-08 01:28:59 +0800

    Changed a few stuff. Made compatible with FF.

---
+++ development/javascript.js
@@ -0,0 +1,3 @@
+$(document).ready(function() {
+  $("#block").pietimer({percent:30});
+});
--- sample/javascript.js
+++ sample/javascript.js
@@ -1,8 +1,11 @@
 $(document).ready(function() {
-  $("#full-pie").pietimer({block:true});
-  $("#new-timer").pietimer({percent:35, withPercent:false, block:true});
+  $("#full-pie").pietimer();
+  $("#new-timer").pietimer({percent:28, withPercent:false, block:true});
+  $("#other-timer").pietimer({percent:61, animate:true});
+  $("#no-closer-timer").pietimer({percent:82, animate:true, withCloser:false});
   $("#delay-timer").pietimer({block:true, duration:10000});
-  $("#other-timer").pietimer({percent:65, animate:true});
+
   $("#another-timer").pietimer('draw', {percent:35});
   $("#another-one-timer").pietimer({percent:73, animate:false, withCloser:false});
+  $("#one-more").pietimer('draw', {percent:52, block:true});
 });
